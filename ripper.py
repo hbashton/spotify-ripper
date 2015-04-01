hbashton/spotify-ripper

commit 96b91b695e45870c4a311da9f57d7742b8612e68

Added an option to automatically remove tracks from a playlist after successful ripping. The option will be ignored if no user defined playlist ist loaded. Does not check if playlist is actually writable, yet.

ripper.py
@@ -52,6 +52,7 @@ class Ripper(threading.Thread):
     pipe = None
     ripping = False
     finished = False
+    current_playlist = None
     end_of_track = threading.Event()
 
     def __init__(self, args):
@@ -102,7 +103,7 @@ class Ripper(threading.Thread):
             tracks = self.search_query(args.uri)
 
         # ripping loop
-        for track in tracks:
+        for idx,track in tracks:
             try:
                 print('Loading track...')
                 track.load()
@@ -129,6 +130,12 @@ class Ripper(threading.Thread):
                 self.end_progress()
                 self.finish_rip(track)
                 self.set_id3_and_cover(track)
+
+                if args.remove_from_playlist and self.current_playlist:
+                    print(Fore.YELLOW + "Removing track from playlist " + self.current_playlist.name + Fore.RESET)
+                    self.current_playlist.remove_tracks(idx)
+                    print('-'*79 + '\n');
+
             except spotify.Error as e:
                 print(Fore.RED + "Spotify error detected" + Fore.RESET)
                 self.logger.error(e)
@@ -145,26 +152,31 @@ class Ripper(threading.Thread):
         link = self.session.get_link(uri)
         if link.type == spotify.LinkType.TRACK:
             track = link.as_track()
-            return iter([track])
-        elif link.type == spotify.LinkType.PLAYLIST or link.type == spotify.LinkType.STARRED:
-            playlist = link.as_playlist()
+            return enumerate([track])
+        elif link.type == spotify.LinkType.PLAYLIST:
+            self.current_playlist = link.as_playlist()
             print('Loading playlist...')
-            playlist.load()
-            return iter(playlist.tracks)
+            self.current_playlist.load()
+            return enumerate(self.current_playlist.tracks)
+        elif link.type == spotify.LinkType.STARRED:
+            starred = link.as_playlist()
+            print('Loading starred playlist...')
+            starred.load()
+            return enumerate(starred.tracks)
         elif link.type == spotify.LinkType.ALBUM:
             album = link.as_album()
             print('Loading album...')
             album.load()
             album_browser = album.browse()
             album_browser.load()
-            return iter(album_browser.tracks)
+            return enumerate(album_browser.tracks)
         elif link.type == spotify.LinkType.ARTIST:
             artist = link.as_artist()
             print('Loading artist...')
             artist.load()
             artist_browser = artist.browse()
             artist_browser.load()
-            return iter(artist_browser.tracks)
+            return enumerate(artist_browser.tracks)
         return iter([])
 
     def search_query(self, query):
@@ -369,6 +381,7 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--overwrite', action='store_true', help='Overwrite existing MP3 files [Default=skip]')
     parser.add_argument('-s', '--strip-colors', action='store_true', help='Strip coloring from output[Default=colors]')
     parser.add_argument('-v', '--vbr', default='0', help='Lame VBR encoding quality setting [Default=0]')
+    parser.add_argument('-r', '--remove-from-playlist', action='store_true', help='Delete tracks from playlist after successful ripping [Default=no]')
     parser.add_argument('uri', help='Spotify URI (either URI, a file of URIs or a search query)')
     args = parser.parse_args()
 

commit b87ebb939332c2bd6182cc3fe6d47cab1991086e

fixed regression in commit 96b91b6

ripper.py
@@ -108,7 +108,7 @@ class Ripper(threading.Thread):
             self.idx_digits = len(str(len(self.current_playlist.tracks)))
 
         # ripping loop
-        for idx, track in tracks:
+        for idx, track in enumerate(tracks):
             try:
                 print('Loading track...')
                 track.load()
@@ -177,31 +177,31 @@ class Ripper(threading.Thread):
         link = self.session.get_link(uri)
         if link.type == spotify.LinkType.TRACK:
             track = link.as_track()
-            return enumerate([track])
+            return iter([track])
         elif link.type == spotify.LinkType.PLAYLIST:
             self.current_playlist = link.as_playlist()
             print('Loading playlist...')
             self.current_playlist.load()
-            return enumerate(self.current_playlist.tracks)
+            return iter(self.current_playlist.tracks)
         elif link.type == spotify.LinkType.STARRED:
             starred = link.as_playlist()
             print('Loading starred playlist...')
             starred.load()
-            return enumerate(starred.tracks)
+            return iter(starred.tracks)
         elif link.type == spotify.LinkType.ALBUM:
             album = link.as_album()
             print('Loading album...')
             album.load()
             album_browser = album.browse()
             album_browser.load()
-            return enumerate(album_browser.tracks)
+            return iter(album_browser.tracks)
         elif link.type == spotify.LinkType.ARTIST:
             artist = link.as_artist()
             print('Loading artist...')
             artist.load()
             artist_browser = artist.browse()
             artist_browser.load()
-            return enumerate(artist_browser.tracks)
+            return iter(artist_browser.tracks)
         return iter([])
 
     def search_query(self, query):