1941373027018fed03f5a6d236fa73177d5d3038
James Newell <james.newell@gmail.com>
2015-09-04 20:30:22 -0400

    bump versions of dependencies and spotify-ripper to 2.4.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def _read(fn):
 
 setup(
     name='spotify-ripper',
-    version='2.4.1',
+    version='2.4.2',
     packages=find_packages(exclude=["tests"]),
     scripts=['spotify_ripper/main.py'],
     include_package_data=True,
@@ -32,9 +32,9 @@ setup(
 
     # Requirements
     install_requires=[
-        'pyspotify==2.0.2',
+        'pyspotify==2.0.3',
         'colorama>=0.3.3',
-        'mutagen==1.29',
+        'mutagen==1.30',
         'requests>=2.3.0',
         'schedule>=0.3.1',
     ],
@@ -47,7 +47,7 @@ setup(
     license='MIT',
     keywords="spotify ripper mp3 ogg vorbis flac opus acc mp4 m4a",
     url='https://github.com/jrnewell/spotify-ripper',
-    download_url='https://github.com/jrnewell/spotify-ripper/tarball/2.4.1',
+    download_url='https://github.com/jrnewell/spotify-ripper/tarball/2.4.2',
     classifiers=[
         'Topic :: Multimedia :: Sound/Audio',
         'Topic :: Multimedia :: Sound/Audio :: Capture/Recording',
