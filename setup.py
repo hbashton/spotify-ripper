5ddddb02296a2e913e39a77082812a7653103176
James Newell <james.newell@gmail.com>
2016-02-25 12:06:46 -0500

    bump version to 2.8.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def _read(fn):
 create_default_dir()
 setup(
     name='spotify-ripper',
-    version='2.7.6',
+    version='2.8.0',
     packages=find_packages(exclude=["tests"]),
     scripts=['spotify_ripper/main.py'],
     include_package_data=True,
@@ -56,7 +56,7 @@ setup(
     license='MIT',
     keywords="spotify ripper mp3 ogg vorbis flac opus acc mp4 m4a",
     url='https://github.com/jrnewell/spotify-ripper',
-    download_url='https://github.com/jrnewell/spotify-ripper/tarball/2.7.6',
+    download_url='https://github.com/jrnewell/spotify-ripper/tarball/2.8.0',
     classifiers=[
         'Topic :: Multimedia :: Sound/Audio',
         'Topic :: Multimedia :: Sound/Audio :: Capture/Recording',
